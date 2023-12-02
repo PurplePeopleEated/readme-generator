59bd22533bd7af1fc32131870e22f1387a9191a3
PurplePeopleEated <karra.maynard@gmail.com>
2023-12-01 19:14:10 -0500

    add initial questions to prompt

--- index.js
+++ index.js
@@ -2,5 +2,24 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 inquirer.prompt([
-
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+  },
+  {
+    type: 'input',
+    name: 'install',
+    message: 'The installation instructions?'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What about usage information?'
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'How about instructions on testing?'
+  },
 ])
