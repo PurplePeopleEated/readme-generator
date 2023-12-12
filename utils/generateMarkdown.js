3257a3cdc031564f4aa9c0243f3975b53e5ce3d4
Karra <karra.maynard@gmail.com>
2023-12-12 18:02:53 -0500

    add licenses to renderLicenseLink

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -23,8 +23,18 @@ function renderLicenseBadge(license) {
 }
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  if (license === 'ISC') {
+    return '[ISC License](https://opensource.org/license/isc-license-txt/)'
+  } else if (license === 'MIT') {
+    return '[MIT License](https://opensource.org/license/mit/)'
+  } else if (license === 'Apache 2.0') {
+    return '[Apache 2.0 License](http://www.apache.org/licenses/LICENSE-2.0)'
+  } else {
+    console.log('No license matched');
+    return '';
+  }
+ }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
