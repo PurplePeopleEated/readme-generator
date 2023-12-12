331001515a547a30533aa2fad0afe1b2dedc1347
Karra <karra.maynard@gmail.com>
2023-12-12 16:21:55 -0500

    initial generateMarkdown

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -21,4 +21,46 @@ function renderLicenseBadge(license) {
       return ''
   }
 }
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) { }
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) { }
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+${renderLicenseBadge(data.license)}
+
+## Installation  
+${data.install}
+
+
+`;
+}
+
+// function example1(){
+//   console.log("something");
+//   return 1+1;
+// }
+
+// function example2(){
+//  let x = example1()
+//  return x + 3
+// }
+
+// function example3(){
+//   let x = example1()
+// }
+// const obj = {
+//   eg1: "example 1",
+//   eg2: "example 2"
+// }
+// array[1]
+// obj.eg1 //'example 1'
+// obj.eg2 //'example 2'
+
 module.exports = generateMarkdown;
